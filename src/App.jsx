f2c2c9023e3edcee7133aa8e08fe39a50d68eee2
yash-bele <yashgbele@gmail.com>
2023-02-09 10:16:03 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import { useState } from 'react';
+import { FiAlertTriangle } from 'react-icons/fi';
+import { FaDesktop } from 'react-icons/fa';
 import Main from './components/main/Main';
 
 const App = () => {
-  const [count, setCount] = useState(0);
-
   return (
-    <main className='font-titillium h-screen select-none'>
-      <Main />
-    </main>
+    <>
+      <main className='font-titillium h-screen select-none hidden lg:block'>
+        <Main />
+      </main>
+      <main className='absolute w-full h-full justify-center items-center flex lg:hidden'>
+        <section className='-mt-32 shadow-md p-5 rounded-md bg-red-100'>
+          <FiAlertTriangle className='text-center text-4xl w-full mb-5 text-red-500' />
+          <h1 className='font-bold text-center text-base text-slate-700'>
+            You are on small screen.
+          </h1>
+          <h1 className='font-bold text-center text-base text-slate-700'>
+            Switch to desktop size screen.
+          </h1>
+          <FaDesktop className='text-center text-4xl w-full mt-5 text-blue-500' />
+        </section>
+      </main>
+    </>
   );
 };
 
